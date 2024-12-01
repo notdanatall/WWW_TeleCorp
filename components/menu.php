9a78b3d4c9f9e845087a98e24887a4a6fa46d7dc
Daniel Pravdik <116765729+notdanatall@users.noreply.github.com>
2024-12-01 10:45:24 +0100

    Konečeně white navmenu Dodělat EMAIL

--- components/menu.php
+++ components/menu.php
@@ -1,19 +1,20 @@
-<nav class="navbar navbar-expand-lg bg-dark border-bottom fixed border-body" data-bs-theme="dark">
+<nav class="navbar navbar-expand-lg bg-dark border-bottom fixed border-body" data-bs-theme="light">
     <div class="container-fluid">
         <!-- Dropdown for the list icon -->
-        <div class="nav-item dropdown">  
-            <a class="nav-link fs-2" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="bi bi-list"
-                style="color: balck;"
-                   onmouseover="this.style.color='darkgrey'" 
-                   onmouseout="this.style.color='black'"></i>
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <li><a class="dropdown-item" href="/../../index.php">Hlavní menu</a></li>
-                <li><a class="dropdown-item" href="/../sites/services.php">Služby</a></li>
-                <li><a class="dropdown-item" href="/../sites/contact.php">Kontaktujte nás</a></li>
-            </ul>
-        </div>
+        <div class="nav-item dropdown">
+    <a class="nav-link fs-2 " href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="bi bi-list"
+           style="color: black;"
+           onmouseover="this.style.color='darkgrey'" 
+           onmouseout="this.style.color='black'"></i>
+    </a>
+    <ul class="dropdown-menu ">
+        <li><a class="dropdown-item" href="/../../index.php"><b>Hlavní menu</b></a></li>
+        <li><a class="dropdown-item" href="/../sites/services.php"><b>Služby</b></a></li>
+        <li><a class="dropdown-item" href="/../sites/contact.php"><b>Kontaktujte nás</b></a></li>
+    </ul>
+</div>
+
         <!-- Logo-->
         <div class="center-content">
                 <a class="navbar-brand" href="index.php">
